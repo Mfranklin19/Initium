c4e569d2f237eee826b37988f14355ec9477d9ea
Michael Franklin <franklm@miamioh.edu>
2017-10-17 13:16:38 -0400

    Commit 2
    Person and hero prefab made, Tile script fleshed out

--- Initium/Assets/Scripts/Tile.cs
+++ Initium/Assets/Scripts/Tile.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    bool settled = false;
+    string owner;
+    string[] occupants = new string[4];
+    int capturePoints = 0;
+    public int neededCP;
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (!settled) isSettled();
 	}
+
+    void isSettled()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (occupants[i] != null) capturePoints++;
+        }
+        if (capturePoints >= neededCP) settled = true;
+        else capturePoints = 0;
+    }
+
 }
